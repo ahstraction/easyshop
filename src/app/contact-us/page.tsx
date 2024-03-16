69d4e0b923dc8db3c71d43aec73de56402e7e77e
salmanamjad902 <developer@innovativemojo.com>
2024-03-16 19:57:53 +0500

    contactus page ui fixes

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -10,13 +10,13 @@ const contactus = () => {
     <>
       <div className="relative ">
         <Image className="h-full w-full absolute z-20" src={bg} alt="" />
-        <div className="absolute bottom-0 z-0 w-full ">
-          <div className="relative overflow-hidden">
+        <div className="absolute h-full bottom-0 z-0 w-full ">
+          <div className="relative overflow-hidden h-full">
             <Image
-              className="object-cover bg-cover bg-no-repeat h-[1100px] w-full   opacity-20"
-              src="/imgs/home/release/featuredbg.png"
+              className="object-cover bg-cover bg-no-repeat h-full w-full   opacity-10"
+              src="/imgs/home/buildings.png"
               alt="background"
-              height={1813}
+              height={2013}
               width={1832}
             />
             <div className="absolute z-10 h-full bottom-0 inset-0 bg-gradient-to-b from-[#121212]  to-transparent opacity-90"></div>
