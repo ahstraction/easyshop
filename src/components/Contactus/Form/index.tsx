426b6aed869662ca2c01edbe8ce2f27bcc97e4bf
salmanamjad902 <developer@innovativemojo.com>
2024-03-16 13:52:59 +0500

    contactus page

--- src/components/Contactus/Form/index.tsx
+++ src/components/Contactus/Form/index.tsx
@@ -6,10 +6,10 @@ import Navbar from "@/components/navbar/Navbar";
 // import Text from "@/components/ui/Text";
 // import Navbar from "@/components/Aboutus/Header/index";
 
-// import fb from "@/imgs/contact/Group 2.svg";
-// import insta from "@/imgs/contact/Group 3.svg";
-// import twiter from "@/imgs/contact/Group 4.svg";
-// import youtube from "@/imgs/contact/Group 5.svg";
+import fb from "@/public/imgs/icons/fb.svg";
+import insta from "@/public/imgs/icons/insta.svg";
+import twiter from "@/public/imgs/icons/twitter.svg";
+import youtube from "@/public/imgs/icons/youtube.svg";
 // import location from "@/imgs/contact/carbon_location-filled.png";
 // import mail from "@/imgs/contact/fluent_mail-16-filled.svg";
 // import bgright from "@/imgs/contact/bgright.png";
@@ -33,7 +33,7 @@ const Form = () => {
     e: React.MouseEvent<HTMLButtonElement, MouseEvent>
   ) => {
     e.preventDefault();
-console.log("clicked")
+    console.log("clicked");
     // if (!fname || !lname || !email || !phone || !message) {
     //   alert("Please fill in all fields");
     //   return;
@@ -80,17 +80,17 @@ console.log("clicked")
     //     throw new Error(data.message || "Failed to send email");
     //   }
     // } catch (error) {
-     
+
     //   console.error("Error sending email:", error);
     //   alert("Failed to send email. Please try again later.");
     // } finally {
-    //   setLoading(false); 
+    //   setLoading(false);
     // }
   };
 
   return (
     <>
-      <div className="bg-[#21201E]  ">
+      <div className=" ">
         <Navbar />
 
         <div className="flex justify-center items-center relative pt-20 pb-32 gradient-background ">
@@ -104,17 +104,16 @@ console.log("clicked")
               height={953.65}
             /> */}
 
-            <div className="flex xl:block  justify-between px-[5%] relative z-20 ">
+            <div className="flex xl:block justify-center items-end  gap-[100px] px-[5%] relative z-20 ">
               {/* left */}
               <div className="xl:flex xl:justify-center">
                 <div className="  w-full max-w-[453px]">
-                  <h1
-                  
-                    className="text-[64px] mob:text-center mob:text-[30px]   text-gradient leading-[62px] font-bold mb-7 mt-3 "
-                  >
-                    Contact Us
+                  <h1 className="text-[60px] mob:text-center mob:text-[30px]  font-outfit  text-gradient leading-[62px] font-medium mb-7 mt-3 ">
+                    Get In Touch
                   </h1>
-                  <p className="text-[16px] mob:text-[14px] mob:leading-[24px] text-[#fff]  leading-[26px] font-normal ">Donec faucibus semper massa, quis congue dolor et. </p>
+                  <p className="text-[18px] mob:text-[14px] mob:leading-[24px] text-[#fff]  leading-[33px] font-jakrata font-normal ">
+                    Donec faucibus semper massa, quis congue dolor et. 
+                  </p>
                   {/* locations London*/}
                   <div className="flex gap-3 mt-16">
                     {/* <div className="img">
@@ -122,18 +121,12 @@ console.log("clicked")
                     </div> */}
 
                     <div className="">
-                      <h1
-                      
-                        className="text-[24px] mob:text-[20px] text-gradient leading-[26px] font-bold mb-2 "
-                      >
-                      Location
+                      <h1 className="text-[18px] mob:text-[q6px] text-gradient leading-[26px] font-semibold font-jakrata mb-2 ">
+                        Location
                       </h1>
 
-                      <h1
-                   
-                        className="text-[16px] mob:text-[14px] mob:leading-[24px] text-[#fff]  leading-[26px] font-normal "
-                      >
-                     7401 Wiles Rd, Coral Springs FL, 33067
+                      <h1 className="text-[16px] mob:text-[14px] mob:leading-[24px] text-[#fff] font-jakrata leading-[33px] font-normal ">
+                        7401 Wiles Rd, Coral Springs FL, 33067
                       </h1>
                       {/* <h1
                         
@@ -149,34 +142,23 @@ console.log("clicked")
                         </span>
                         +44 (20) 3666 5000
                       </h1> */}
-
-                     
                     </div>
                   </div>
 
                   {/* dc*/}
-                  <div className="flex gap-3 mt-8">
+                  <div className="flex gap-3 mt-9">
                     {/* <div className="img">
                       <Image src={location} alt="" width={80} height={80} />
                     </div> */}
 
                     <div className="">
-                      <h1
-                   
-                        className="text-[24px] mob:text-[20px] text-gradient leading-[26px] font-bold mb-2 "
-                      >
-                Phone Number
+                      <h1 className="text-[18px] mob:text-[q6px] text-gradient leading-[26px] font-semibold font-jakrata mb-2 ">
+                        Phone Number
                       </h1>
 
-                      <h1
-                     
-                        className="text-[16px] mob:text-[14px] mob:leading-[24px] text-[#fff] leading-[26px] font-normal "
-                      >
-                       949-981-2225
+                      <h1 className="text-[16px] mob:text-[14px] mob:leading-[24px] text-[#fff] font-jakrata leading-[33px] font-normal ">
+                        949-981-2225
                       </h1>
-
-                      
-                      
                     </div>
                   </div>
 
@@ -187,32 +169,23 @@ console.log("clicked")
                     </div> */}
 
                     <div className="">
-                      <h1
-                      
-                        className="text-[24px] mob:text-[20px] text-gradient leading-[26px] font-bold mb-2 "
-                      >
-                     Email Address
+                      <h1 className="text-[18px] mob:text-[q6px] text-gradient leading-[26px] font-semibold font-jakrata mb-2 ">
+                        Email Address
                       </h1>
 
-                      <h1
-                        
-                        className="text-[16px] mob:text-[14px] mob:leading-[24px] text-[#fff] leading-[26px] font-normal "
-                      >
-                  Bop@philliebopmusic.com
+                      <h1 className="text-[16px] mob:text-[14px] mob:leading-[24px] text-[#fff] font-jakrata leading-[33px] font-normal ">
+                        Bop@philliebopmusic.com
                       </h1>
-
-                  
-
                     </div>
                   </div>
 
                   {/* social icons */}
-                  <div className="flex w-full xl:justify-center gap-[20px] mt-12 hidden">
+                  <div className="flex w-full xl:justify-center gap-[20px] mt-16 ">
                     {/* facebook */}
-                    {/* <Image src={fb} alt="" width={43} height={43} />
+                    <Image src={fb} alt="" width={43} height={43} />
                     <Image src={insta} alt="" width={43} height={43} />
                     <Image src={twiter} alt="" width={43} height={43} />
-                    <Image src={youtube} alt="" width={43} height={43} /> */}
+                    <Image src={youtube} alt="" width={43} height={43} />
                   </div>
                 </div>
               </div>
@@ -220,34 +193,33 @@ console.log("clicked")
               {/* right */}
 
               <div className="xl:flex xl:justify-center">
-                <div className=" justify-center w-full xl:mt-10 max-w-[589px] p-10 bg-[#131211] rounded-[20px] max-h-[590px] xl:max-h-full">
-                
-                
-
+                <div className=" justify-center w-full xl:mt-10 max-w-[530px] px-16 py-20 bg-[#131211]  max-h-[564px] xl:max-h-full">
+                  <label
+                    htmlFor="First Name"
+                    className="text-[16px] text-[#FFFFFF] leading-[20.16px] font-normal font-jakrata"
+                  >
+                    First Name
+                  </label>
                   <input
-                    className="w-full px-6 h-[45px]  outline-none border-b bg-transparent border-b-[#FFFFFF] text-[16px] text-[#FFFFFF] font-normal mb-6 "
+                    className="w-full  h-[25px]  outline-none border-b bg-transparent border-b-[#FFFFFF] text-[16px] text-[#FFFFFF] font-normal mb-6 "
                     type="text"
                     value={fname}
                     onChange={(e) => {
                       setFname(e.target.value);
                     }}
-                    placeholder="First Name"
+                    // placeholder="First Name"
                   />
 
+                  <label
+                    htmlFor="Email address"
+                    className="text-[16px] text-[#FFFFFF] leading-[20.16px] font-normal font-jakrata"
+                  >
+                    Email address
+                  </label>
                   <input
-                  className="w-full px-6 h-[45px]  outline-none border-b bg-transparent border-b-[#FFFFFF] text-[16px] text-[#FFFFFF] font-normal mb-6 "
-                    type="text"
-                    value={lname}
-                    onChange={(e) => {
-                      setLname(e.target.value);
-                    }}
-                    placeholder="Last Name"
-                  />
-
-                  <input
-                   className="w-full px-6 h-[45px]  outline-none border-b bg-transparent border-b-[#FFFFFF] text-[16px] text-[#FFFFFF] font-normal mb-6 "
-                    type="email"
-                    placeholder="Email"
+                    className="w-full  h-[25px]  outline-none border-b bg-transparent border-b-[#FFFFFF] text-[16px] text-[#FFFFFF] font-normal mb-6 "
+                    type="Email address"
+                    // placeholder="Email"
                     value={email}
                     onChange={(e) => {
                       setEmail(e.target.value);
@@ -256,39 +228,48 @@ console.log("clicked")
                   />
                   {emailError && <p style={{ color: "red" }}>{emailError}</p>}
 
+                  <label
+                    htmlFor="Mobile number"
+                    className="text-[16px] text-[#FFFFFF] leading-[20.16px] font-normal font-jakrata"
+                  >
+                    Mobile number
+                  </label>
                   <input
-                    className="w-full px-6 h-[45px]  outline-none border-b bg-transparent border-b-[#FFFFFF] text-[16px] text-[#FFFFFF] font-normal mb-6 "
+                    className="w-full  h-[25px]  outline-none border-b bg-transparent border-b-[#FFFFFF] text-[16px] text-[#FFFFFF] font-normal mb-6 "
                     type="number"
                     value={phone}
                     onChange={(e) => {
                       setPhone(e.target.value);
                     }}
-                    placeholder="Phone Number"
+                    // placeholder="Phone Number"
                   />
 
+                  <label
+                    htmlFor="Message"
+                    className="text-[16px] text-[#FFFFFF] leading-[20.16px] font-normal font-jakrata"
+                  >
+                    Message
+                  </label>
                   <textarea
-  className="w-full px-6 h-[45px]  outline-none border-b bg-transparent border-b-[#FFFFFF] text-[16px] text-[#FFFFFF] font-normal mb-6 "
+                    className="w-full  h-[25px]  outline-none border-b bg-transparent border-b-[#FFFFFF] text-[16px] text-[#FFFFFF] font-normal mb-6 "
                     value={message}
                     onChange={(e) => {
                       setMessage(e.target.value);
                     }}
-                    placeholder="Message"
+                    // placeholder="Message"
                   ></textarea>
-
 
                   <button
                     type="submit"
-                    onClick={sendMail}
+                    // onClick={sendMail}
                     className=" py-[15px] px-[30px] rounded-[150px] bg-[#FFFFFF] text-[#121212] text-[15px] leading-[18.9px] font-semibold font-jakrataS"
                     disabled={loading} // Disable button if loading state is true
                   >
-                    {loading ? "Sending..." : "Submit"}
+                    {/* {loading ? "Sending..." : "Submit"} */}
+                    Submit
                   </button>
                   {successMessage && (
-                    <h1
-                     
-                      className="text-[24px] font-bold text-green-500 leading-7 text-center pt-4"
-                    >
+                    <h1 className="text-[24px] font-bold text-green-500 leading-7 text-center pt-4">
                       {successMessage}
                     </h1>
                   )}
